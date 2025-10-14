5f830a4f0bfd868312e5e45c5e8498f7482c1607
marianela-gomez <marianela.gomez.linkedin@gmail.com>
2025-10-14 18:00:44 +0200

    first commit

---
+++ get_measurements.py
@@ -0,0 +1,17 @@
+from machine import ADC, Pin
+import utime
+import statistics
+
+
+def get_capacitator_measurements_2():
+    soil_adc = ADC(Pin(26))
+    five_measurements = []
+    while len(five_measurements) <= 5:
+        five_measurements.append(soil_adc.read_u16())
+        utime.sleep(3)
+    return statistics.mean(five_measurements), statistics.stdev(five_measurements)
+
+my_measure = get_capacitator_measurements_2()
+
+with open('out.txt', 'a') as f:
+    f.write(str(my_measure))
