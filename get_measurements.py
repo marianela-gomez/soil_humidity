c65fd2fc3856fe67b23f7944a781157f404e7127
marianela-gomez <marianela.gomez.linkedin@gmail.com>
2025-10-14 18:18:24 +0200

    corrections in measurements function

--- get_measurements.py
+++ get_measurements.py
@@ -3,7 +3,7 @@ import utime
 import statistics
 
 
-def get_capacitator_measurements_2():
+def get_capacitator_measurements():
     soil_adc = ADC(Pin(26))
     five_measurements = []
     while len(five_measurements) <= 5:
@@ -11,7 +11,7 @@ def get_capacitator_measurements_2():
         utime.sleep(3)
     return statistics.mean(five_measurements), statistics.stdev(five_measurements)
 
-my_measure = get_capacitator_measurements_2()
+my_measure = get_capacitator_measurements()
 
 with open('out.txt', 'a') as f:
     f.write(str(my_measure))
